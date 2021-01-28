bff0a716d92a6c215e004c05a43469ab51cbfb59
kyrastyl <kyrastyl@csd.auth.gr>
2021-01-29 00:46:36 +0200

    Code Refactored and Plots organized
    Also added function that creates a wordCloud from words extracted of our tweets

--- pipeline.py
+++ pipeline.py
@@ -7,71 +7,22 @@ Created on Sun Dec 27 14:30:30 2020
 """
 import time
 import sys
-#from word2vec import W2V
+from word2vec import W2V
 import preprocessing as pp
 import dataloader as dl
 import fasttextpp as ftp
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 from sklearn.naive_bayes import MultinomialNB
-
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, balanced_accuracy_score, confusion_matrix
 import re
 import numpy as np
 import matplotlib.pyplot as plt
+import wordCloud as wc
+from nltk.tokenize import word_tokenize
+import plotslib as pl
 
 
-def calculateCOUNTS(y):
-    countsPos=0
-    countsNeu=0
-    countsNeg=0
-    
-    for target in y:
-        target = ftp.FastTextModel(quiet=True).mapToLabel(target)
-        if target=="POSITIVE":
-            countsPos+=1
-        elif target=="NEUTRAL":
-            countsNeu+=1
-        elif target=="NEGATIVE":
-            countsNeg+=1
-        #else:
-           # print("Unknown Target!")
-    return [countsNeg, countsNeu, countsPos]
-
-def plotCOUNTS(y_train, y_test,factor):
-    labels = ['NEGATIVE', 'NEUTRAL', 'POSITIVE']
-    means1 = [c*factor for c in calculateCOUNTS(y_train)]
-    means2 = [c*factor for c in calculateCOUNTS(y_test)]
-    
-    x = np.arange(len(labels))  # the label locations
-    width = 0.35  # the width of the bars
-    
-    fig, ax = plt.subplots()
-    rects1 = ax.bar(x - width/2, means1, width, label='Train')
-    rects2 = ax.bar(x + width/2, means2, width, label='Test')
-    
-    # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel('Counts')
-    ax.set_title('Counts by set and class')
-    ax.set_xticks(x)
-    ax.set_xticklabels(labels)
-    ax.legend()
-    
-    
-    def autolabel(rects):
-        for rect in rects:
-            height = rect.get_height()
-            ax.annotate('{}'.format(height),
-                        xy=(rect.get_x() + rect.get_width() / 2, height),
-                        xytext=(0, 3),  # 3 points vertical offset
-                        textcoords="offset points",
-                        ha='center', va='bottom')
-    autolabel(rects1)
-    autolabel(rects2)
-    
-    fig.tight_layout()
-    
-    plt.show()
     
 def score(model, X_test, y_test):
     y_pred = model.predict(X_test)
@@ -84,41 +35,29 @@ def score(model, X_test, y_test):
     'recall': recall_score(y_test, y_pred, average='macro'), 'f1': f1_score(y_test, y_pred, average='macro')}
     return results
 
-
 args = sys.argv
 
-#print(len(args))
+print(len(args))
 print(args)
 
-if(len(args)==1):
+if(len(args)<1):
     print("No arguments passed!\nTry again.")
     sys.exit()
 else:
-
-    file = args[1]
+    file = args[0]
     
     print("Instantiating model...")
-    
-    load_model = args[2]
-    representation = args[3]
 
-    #file = args[0]
-    file = "dataset_stella.csv"
-    
-    print("Instantiating model...")
-    
-    load_model = "Fasttext"
-    representation = "tfidf"
     model = None
-
     parameters = {}
-    load_model = re.sub('[ ]*','',load_model).lower()
+    load_model = re.sub('[ ]*','',args[1]).lower()
+    representation = re.sub('[ ]*','',args[2]).lower()
     
     if load_model=="fasttext":
         parameters = {"lr":0.1,"epoch":500,"wordNgrams":3,"dim":20} #fasttext parameters
         model = ftp.FastTextModel(**parameters)
     elif load_model=="logisticregression":
-        parameters = {"penalty":'l2',"C":0.1,"solver":'lbfgs','max_iter':200} #lr parameters
+        parameters = {"penalty":'l2',"C":0.1,"solver":'lbfgs'} #lr parameters
         model = LogisticRegression(**parameters)
     elif load_model=="svm":
         parameters = {"kernel":'rbf', "gamma":'scale', "C":100} #svm parameters
@@ -131,18 +70,29 @@ else:
     
     
     # load dataset
-
-    dataloader = dl.DataLoader(file, int(args[4]))
-    #dataloader = dl.DataLoader(file, 500000)
-    X,y = dataloader.load_dataset(return_X_y=True)
+    dataloader = dl.DataLoader(file, args[3])
+    X_old,y = dataloader.load_dataset(return_X_y=True)
     print("Dataset loaded")
 
+    startPrep = time.time()
     #preprocess tweets
-    #X = [' '.join(pp.preprocess(tweet)) for tweet in X]
+    words = []
+    X = []
+    for tweet in X_old:
+        wordsTemp = pp.preprocess(tweet)
+        for word in wordsTemp:
+            words.append(word)
+            X.append(' '.join(wordsTemp))
+            
+    stopPrep = time.time()
+    
+    # create a wordCloud with tweets
+    # you can define the max words (default = 2000)
+    pl.createWordCloud(words)
     
     #if not fasttext convert score to labels
-    #if load_model!='fasttext':
-     #   y = [ftp.FastTextModel(quiet=True).mapToLabel(score) for score in y]
+    if load_model!='fasttext':
+        y = [ftp.FastTextModel(quiet=True).mapToLabel(score) for score in y]
     
     #re-set the tweets and scores in dataloader (after changes)
     dataloader.set_X(X,y)
@@ -150,8 +100,7 @@ else:
     # split to train and test
     print("Train-test split complete")
     X_train, X_test, y_train, y_test = dataloader.train_test_split(X, y, 0.7)
-
-    plotCOUNTS(y_train, y_test, 2)    
+    
     
     # if not fasttext get tfidf
     if load_model!='fasttext':
@@ -169,25 +118,22 @@ else:
     print("Fitting the model...")
     model.fit(X_train,y_train)
 
-    print("Training metrics")
-    sc = score(model, X_train, y_train)
     if load_model=='fasttext':
         y_train = [model.transform_instance(score) for score in y_train]
         y_test = [model.transform_instance(score) for score in y_test]
     
-    print("Score...")
+    print("Score model in training dataset...")
     sc = score(model, X_train, y_train)
+    
+    pl.plotCOUNTS(y_train, y_test)
     
     print("Metrics of ",load_model," are:", sc)
 
-    print()
-
-    print("Testing metrics")
+    print("Score model in test dataset...")
     sc = score(model, X_test, y_test)
-
+    
     print("Metrics of ",load_model," are:", sc)
     
     stopTime = time.time()
-    print("Execution Took : ",stopTime - startTime," seconds.")
-
-    print()
+    print('Preprocess Took : ', stopPrep - startPrep, 'seconds.')
+    print('Train and test Took : ',stopTime - startTime,' seconds.')
---
+++ plotslib.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 00:07:21 2021
+
+@author: kyrastyl
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from wordcloud import WordCloud
+import fasttextpp as ftp
+
+"""
+    Creates a WordCloud based on a specific image.
+"""
+def createWordCloud(words,maxWords=2000):
+    
+    mask = np.array(Image.open('myCovidBanner.jpg'))
+    
+    wc = WordCloud(background_color="white", max_words=maxWords, mask=mask)
+    clean_string = ','.join(words)
+    wc.generate(clean_string)
+
+    plt.figure(figsize=(50,50))
+    plt.imshow(wc, interpolation='bilinear')
+    plt.title('Covid-19 WordCloud', size=40)
+    plt.axis("off")
+    plt.show()
+    
+
+def calculateCOUNTS(y):
+    countsPos=0
+    countsNeu=0
+    countsNeg=0
+    
+    for target in y:
+        target = ftp.FastTextModel(quiet=True).mapToLabel(target)
+        if target=="POSITIVE":
+            countsPos+=1
+        elif target=="NEUTRAL":
+            countsNeu+=1
+        elif target=="NEGATIVE":
+            countsNeg+=1
+        #else:
+           # print("Unknown Target!")
+    return [countsNeg, countsNeu, countsPos]
+
+
+"""
+    Creates a paired plot with counts of neutral, negative and positive tweets.
+"""
+def plotCOUNTS(y_train, y_test):
+    labels = ['NEGATIVE', 'NEUTRAL', 'POSITIVE']
+    means1 = [c for c in calculateCOUNTS(y_train)]
+    means2 = [c for c in calculateCOUNTS(y_test)]
+    
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+    
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, means1, width, label='Train')
+    rects2 = ax.bar(x + width/2, means2, width, label='Test')
+    
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Counts')
+    ax.set_title('Counts by set and class')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+    
+    
+    def autolabel(rects):
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
+    autolabel(rects1)
+    autolabel(rects2)
+    
+    fig.tight_layout()
+    
+    plt.show()
